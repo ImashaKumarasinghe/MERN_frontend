f5232d8b1dfed4908cc3308173896f2900e3caaf
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-10-31 20:29:27 +0530

    Initial commit - Cosmatis frontend

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import UserData from "./userData"
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Grand Cosmetics</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum maxime quae, esse enim culpa at eveniet delectus dignissimos optio neque? Fugit accusamus perspiciatis quia suscipit debitis sequi atque totam commodi.</p>
+      <UserData />
+    </header>
+  )
+}
