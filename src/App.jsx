65ab313d4a210ca52f96331bde5ae008cf7b1a51
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-11-01 15:24:37 +0530

    refactor: remove Header component and simplify App structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ function App() {
   
 
   return (
-  <div className='w-[100px] h-[100px] bg-blue-500'>
-
+  <div className='w-full h-screen bg-red-500 flex-col justify-center items-center flex'>
+    <div className='w-500 h-500 bg-green-400 flex-col justify-center items-center flex '>
+      <div className='w-[100px] h-[100px] bg-blue-500'></div>
+      <div className='w-[100px] h-[100px] bg-pink-500'></div>
+    </div>
   </div>)
 }
 
