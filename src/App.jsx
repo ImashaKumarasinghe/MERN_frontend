ffd79bf3fe85ba9a8504222b4134edb288021600
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-11-01 19:16:48 +0530

    add test routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/login'
 import SignUpPage from './pages/signup'
 import HomePage from './pages/home'
 import AdminPage from './pages/adminPage'
+import TestPage from './pages/testPage'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/admin" element={<AdminPage />} />
+          <Route path="/test" element={<TestPage />} />
           <Route path='/*' element={<h1>404 Not Found</h1>} />
           
         </Routes>
