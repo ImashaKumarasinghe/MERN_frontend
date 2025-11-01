de4ebaab243e2488ec2ddbc44d6c5419c7905926
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-11-01 13:15:56 +0530

    feat: integrate Tailwind CSS and update styles
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated the Header component to use Tailwind CSS for styling.
    - Removed existing CSS styles in index.css and imported Tailwind CSS.
    - Configured Vite to use the Tailwind CSS plugin.
    - Created a new productCard component file.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,10 +2,11 @@ import UserData from "./userData"
 
 export default function Header() {
   return (
-    <header>
+    <div className="bg-red-500 p-4 text-center">
+
       <h1>Grand Cosmetics</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum maxime quae, esse enim culpa at eveniet delectus dignissimos optio neque? Fugit accusamus perspiciatis quia suscipit debitis sequi atque totam commodi.</p>
       <UserData />
-    </header>
+    </div>
   )
 }
