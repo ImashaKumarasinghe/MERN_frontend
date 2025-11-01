1f65a4883c6a2a5f25e1145348d355996727a610
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-11-01 16:13:48 +0530

    update package dependencies and clean up unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header'
-import Login from './pages/login'
+import LoginPage from './pages/login'
+import SignUpPage from './pages/signup'
+import HomePage from './pages/home'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-red-100'>
-      <Header />
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
