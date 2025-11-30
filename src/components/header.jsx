13c5696fcc698259c5c77fb94682a3ef05de1d50
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-11-30 20:01:05 +0530

    Add logo image and update Header component layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,21 @@
-
+import { Link, useNavigate } from "react-router-dom";
+import UserData from "./userData";
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
-    <div> 
-
+     <header className="w-full h-[80px] shadow-2xl flex justify-center relative ">
+   
+        <img src="/logo.png" alt="Logo" className="w-[80px] h-[80px] object-cover cursor-pointer" />
+        <div className="w-[calc(100%-160px)] h-full hidden md:flex justify-center items-center">
+                <Link to="/" className=" text-[20px] font-bold mx-2">Home</Link>
+                <Link to="/products" className=" text-[20px] font-bold mx-2">Products</Link>
+                <Link to="/about" className=" text-[20px] font-bold mx-2">About</Link>
+                <Link to="/contact" className=" text-[20px] font-bold mx-2">Contact</Link>
+                <Link to="/search" className=" text-[20px] font-bold mx-2">Search</Link>
+            </div>
       
-    </div>
+     </header>
+    
   )
 }
