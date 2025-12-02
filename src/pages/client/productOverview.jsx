2a107194b23cb270af388d974d5f759d4ea74685
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-02 21:54:48 +0530

    Update ProductOverviewPage to display dynamic product ID in overview message

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -1,6 +1,12 @@
+import { useParams } from 'react-router-dom';
+
 export default  function ProductOverviewPage() {
+    const params = useParams();
+    const productId = params.Id;
+
+    
   return (
     <div>
-        this is overview page
+        this is overview page for product {productId}
     </div>
   )}
