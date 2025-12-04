ad9744fa4b06b9d3ca30e710e94f634c1abd5985
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-04 18:07:26 +0530

    Add getCart function to manage cart data in localStorage

---
+++ src/utils/cart.js
@@ -0,0 +1,11 @@
+export function getCart() {
+	let cart = localStorage.getItem("cart");
+    
+	if (cart == null) {
+		cart = [];
+		localStorage.setItem("cart", JSON.stringify(cart));
+	}else{
+        cart = JSON.parse(cart);
+    }
+	return cart;
+}
