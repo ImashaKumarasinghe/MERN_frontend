6d5cfd573a73ee26bafa070d4c4e66c86eb1ba4d
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-06 18:40:55 +0530

    Add AdminOrdersPage component for orders management

---
+++ src/pages/admin/adminOrdersPage.jsx
@@ -0,0 +1,9 @@
+export default function AdminOrdersPage() {
+    return (
+        <div className="w-full h-screen flex font-[Poppins] bg-[#F9F5F7] text-[#3D2A34]">
+            
+                Orders Management
+           
+        </div>
+    )
+}
