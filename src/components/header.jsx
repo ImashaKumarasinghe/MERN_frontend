08d9577ec6603b27f320c6574c856e5689ab950f
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-07 16:24:23 +0530

    Refactor logo image element for cleaner code structure

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,12 +10,9 @@ export default function Header() {
     <header className="w-full h-[80px] shadow-2xl flex justify-center items-center relative px-4">
       <GiHamburgerMenu className ="h-full mx-2 text-3xl md:hidden absolute left-4" />
 
-      <img 
-        src="/logo.png" 
-        alt="Logo" 
-        className="w-[80px] h-[80px] object-cover cursor-pointer absolute left-4" 
-        onClick={() => navigate("/")}
-      />
+       <img onClick={()=>{
+                navigate("/")
+            }} src="/logo.png" alt="Logo" className="w-[80px] h-[80px] object-cover cursor-pointer"/>
       
       <div className="w-[calc(100%-160px)] h-full hidden md:flex justify-center items-center">
         <Link to="/" className="text-[20px] font-bold mx-2 hover:text-blue-600 transition">
