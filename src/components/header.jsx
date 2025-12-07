adb9e6826e8f59a0cf8852667240b5c13133c78c
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-07 16:16:06 +0530

    Fix styling for hamburger menu icon in Header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
   
   return (
     <header className="w-full h-[80px] shadow-2xl flex justify-center items-center relative px-4">
-      <GiHamburgerMenu className ="" />
+      <GiHamburgerMenu className ="h-full mx-2 text-3xl" />
       <img 
         src="/logo.png" 
         alt="Logo" 
