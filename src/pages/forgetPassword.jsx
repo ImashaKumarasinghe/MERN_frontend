3d7979db848f2d926b00e7c1aea0f36258eda024
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-09 11:55:43 +0530

    Add state management for ForgetPasswordPage component

--- src/pages/forgetPassword.jsx
+++ src/pages/forgetPassword.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export default function ForgetPasswordPage() {
+    const [otpSent, setotp] = useState(false)
+    const [email, setEmail] = useState("")
+    const [otp, setOtp] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-50">
       
