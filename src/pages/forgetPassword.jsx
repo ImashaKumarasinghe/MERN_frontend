45dfdeeac4708eeffdf7f48cc9fa88d6c36ae71a
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-09 11:34:50 +0530

    Add ForgetPasswordPage component

---
+++ src/pages/forgetPassword.jsx
@@ -0,0 +1,7 @@
+export default function ForgetPasswordPage() {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-50">
+      
+    </div>
+  );
+}
